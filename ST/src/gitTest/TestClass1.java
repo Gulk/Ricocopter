9851e0f96ae2f206531fcd92d8ea6e498d11504c
avinerry <avinerry@avinerry>
2016-01-14 11:02:25 +0100

    LOOOL

--- ST/src/gitTest/TestClass1.java
+++ ST/src/gitTest/TestClass1.java
@@ -4,7 +4,7 @@ public class TestClass1 {
 
 	public static void main(String[] args) {
 		if(2 > 3) {
-			;
+			System.out.println("LOOOOLLL");
 		} else {
 			System.out.println("Hallo Pascal");
 		}
